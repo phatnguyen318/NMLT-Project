e5e8c72e5d798a494b7d54b4e56ca78c13414742
Phat Nguyen Trien <nguyentrienphat@gmail.com>
2021-05-09 21:10:30 +0700

    add b feature-Phat

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace AppQLBH
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int b = 0;
+        }
     }
 }
